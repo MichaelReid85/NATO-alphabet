80b4e2480b4afaef2492dcadde1fe310fa427c63
MichaelReid85 <Mike1109r@yahoo.com>
2023-12-03 10:50:20 -0800

    added try/except functionality.

--- NATO-alphabet-start/main.py
+++ NATO-alphabet-start/main.py
@@ -4,7 +4,17 @@ data = pandas.read_csv("nato_phonetic_alphabet.csv")
 
 nato_alphabet = {row.letter: row.code for (index, row) in data.iterrows()}
 
-word = input("What is your word?: ").upper()
-output_list = word_to_nato = [nato_alphabet[letter] for letter in word]
 
-print(word_to_nato)
+def generate_phonetic():
+    word = input("What is your word?: ").upper()
+    try:
+        output_list = word_to_nato = [nato_alphabet[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(output_list)
+        generate_phonetic()
+
+
+generate_phonetic()
